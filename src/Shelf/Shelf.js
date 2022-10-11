852ee45c2cd9326388c6a51b57a439b3239c3509
RussellShire <russell.shire@gmail.com>
2022-10-11 14:41:49 +0100

    Add clean-up of comments and spacing

--- src/Shelf/Shelf.js
+++ src/Shelf/Shelf.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Book from '../Book/Book';
 import * as BooksAPI from '../BooksAPI'
 
-
-
 function Shelf({ shelfName, shelfId }){
     const [books, setBooks] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
@@ -47,19 +45,6 @@ function Shelf({ shelfName, shelfId }){
                                 </li>
                             )}
                         )}
-                        
-                        {/* <li>
-                            <Book 
-                            bookTitle='To Kill a Mockingbird' 
-                            bookAuthors='Harper Lee' 
-                            bookCover='http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api' />
-                        </li>
-                        <li>
-                            <Book 
-                            bookTitle='Enders Game' 
-                            bookAuthors='Orson Scott Card' 
-                            bookCover='http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api' />
-                        </li> */}
                     </ol>
                 </div>
         </div>
